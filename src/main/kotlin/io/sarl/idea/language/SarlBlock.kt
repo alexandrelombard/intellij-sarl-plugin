f9dfefe0c4cc9196110e65ad0cc6447601ad088d
Alexandre Lombard <alexandre.lombard@utbm.fr>
2020-07-17 17:20:23 +0200

    Save commit

--- src/main/kotlin/io/sarl/idea/language/SarlBlock.kt
+++ src/main/kotlin/io/sarl/idea/language/SarlBlock.kt
@@ -4,6 +4,8 @@ import com.intellij.formatting.*
 import com.intellij.lang.ASTNode
 import com.intellij.psi.TokenType
 import com.intellij.psi.formatter.common.AbstractBlock
+import io.sarl.idea.language.psi.SarlAgentDeclaration
+import io.sarl.idea.language.psi.SarlStatementList
 
 class SarlBlock(node: ASTNode, wrap: Wrap?, alignment: Alignment?, private val spacingBuilder: SpacingBuilder) :
         AbstractBlock(node, wrap, alignment) {
@@ -13,8 +15,11 @@ class SarlBlock(node: ASTNode, wrap: Wrap?, alignment: Alignment?, private val s
         var child = myNode.firstChildNode
         while (child != null) {
             if(child.elementType != TokenType.WHITE_SPACE) {
-                val block = SarlBlock(child, Wrap.createWrap(WrapType.NONE, false),
-                        Alignment.createAlignment(), spacingBuilder)
+                val block = SarlBlock(
+                        child,
+                        Wrap.createWrap(WrapType.NONE, false),
+                        Alignment.createAlignment(),
+                        spacingBuilder)
                 blocks.add(block)
             }
             child = child.treeNext
@@ -31,7 +36,12 @@ class SarlBlock(node: ASTNode, wrap: Wrap?, alignment: Alignment?, private val s
     }
 
     override fun getIndent(): Indent {
-        return Indent.getNoneIndent()
+        when(node.treeParent?.psi) {
+            is SarlAgentDeclaration,
+            is SarlStatementList ->
+                return Indent.getContinuationIndent()
+            else -> return Indent.getNoneIndent()
+        }
     }
 
 }
