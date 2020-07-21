12dfb10edf264ba767e28b311a1a8b200c04943b
Alexandre Lombard <alexandre.lombard@utbm.fr>
2020-07-21 15:50:52 +0200

    Working on references/generics

--- src/main/kotlin/io/sarl/idea/language/SarlCompletionContributor.kt
+++ src/main/kotlin/io/sarl/idea/language/SarlCompletionContributor.kt
@@ -4,18 +4,58 @@ import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.util.ProcessingContext
+import io.sarl.idea.SarlLanguage
+import io.sarl.idea.language.psi.SarlElementType
 import io.sarl.idea.language.psi.SarlTypes
 
 class SarlCompletionContributor : CompletionContributor() {
 
     init {
-        // FIXME Not working
-        extend(CompletionType.BASIC, PlatformPatterns.psiElement(SarlTypes.IMPORT_DECLARATIONS),
+        extend(CompletionType.BASIC, PlatformPatterns.psiElement().inside(false, PlatformPatterns.psiElement(SarlTypes.PACKAGE_DECLARATION)),
                 object : CompletionProvider<CompletionParameters>() {
                     override fun addCompletions(parameters: CompletionParameters,
-                                       context: ProcessingContext,
-                                       resultSet: CompletionResultSet) {
-                        resultSet.addElement(LookupElementBuilder.create("import"))
+                                                context: ProcessingContext,
+                                                resultSet: CompletionResultSet) {
+                        // TODO Suggest package name (according to directory name)
+                        println("package")
+                        resultSet.addElement(LookupElementBuilder.create(parameters.originalFile.containingDirectory))
+                    }
+                }
+        )
+
+        extend(CompletionType.BASIC, PlatformPatterns.psiElement().inside(false, PlatformPatterns.psiElement(SarlTypes.IMPORT_DECLARATION)),
+                object : CompletionProvider<CompletionParameters>() {
+                    override fun addCompletions(parameters: CompletionParameters,
+                                                context: ProcessingContext,
+                                                resultSet: CompletionResultSet) {
+                        // TODO Suggest imports
+                        println("import")
+                    }
+                }
+        )
+
+        // FIXME
+        extend(CompletionType.BASIC, PlatformPatterns.psiElement().inside(false, PlatformPatterns.psiElement(SarlTypes.CLASS_DECLARATION)),
+                object : CompletionProvider<CompletionParameters>() {
+                    override fun addCompletions(parameters: CompletionParameters,
+                                                context: ProcessingContext,
+                                                resultSet: CompletionResultSet) {
+                        resultSet.addElement(LookupElementBuilder.create(parameters.originalFile.name))
+                    }
+                }
+        )
+
+        // FIXME Not working
+        extend(CompletionType.SMART, PlatformPatterns.psiElement().withSuperParent(10, PlatformPatterns.psiElement(SarlTypes.CLASS_BODY)),
+                object : CompletionProvider<CompletionParameters>() {
+                    override fun addCompletions(parameters: CompletionParameters,
+                                                context: ProcessingContext,
+                                                resultSet: CompletionResultSet) {
+                        resultSet.addElement(LookupElementBuilder.create("def"))
+                        resultSet.addElement(LookupElementBuilder.create("new"))
+                        resultSet.addElement(LookupElementBuilder.create("val"))
+                        resultSet.addElement(LookupElementBuilder.create("var"))
+                        println("class body")
                     }
                 }
         )
